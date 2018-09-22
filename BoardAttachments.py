43748918acf858b12f155f8d1954907af4963019
Zach L'Anglois <zach.langlois@parivedasolutions.com>
2018-09-21 21:55:30 -0500

    first commit

--- BoardAttachments.py
+++ BoardAttachments.py
@@ -9,9 +9,9 @@ def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
 def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
 
 
-api_key = '5d961ca71bfc7a390e96c18ebae24792'
-oauth_token = 'eca5636d148d8465ae5c05c0cb81ed941df378a37ae98ed7415f2d53f2ebb36e'
-board_id = '53b3707ff8537f939d782a92'
+api_key = ''
+oauth_token = ''
+board_id = ''
 
 response = requests.get(
     'https://api.trello.com/1/boards/' + board_id + '/cards?key=' + api_key + '&token=' + oauth_token)
